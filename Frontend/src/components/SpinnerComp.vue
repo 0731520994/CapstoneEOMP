aa5c879deedc9fabfcb4116f841fe67988e36734
Mongy <mongidlanjwa@gmail.com>
2023-08-31 14:17:04 +0200

    update

---
+++ Frontend/src/components/SpinnerComp.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="d-flex justify-content-center">
+      <div class="spinner-border" role="status">
+        <span>Loading...</span>
+      </div>
+    </div>
+  </template>
+
+   
+<style>
+.spinner-border{
+  background: #000;
+  width: 100px;
+}
+
+</style>

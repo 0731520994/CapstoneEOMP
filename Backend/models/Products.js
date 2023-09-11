31a6faf4c7f0d9fd10fff60fee292d4bc9acf268
Mongy <mongidlanjwa@gmail.com>
2023-09-11 12:02:59 +0200

    add

--- Backend/models/Products.js
+++ Backend/models/Products.js
@@ -33,48 +33,48 @@ class Products{
         });
     }
     
-    // addProduct(req, res) {
-    //   const query = `
-    //           INSERT INTO Products 
-    //           SET ?
-    //       `;
-    //   db.query(query, [req.body], (err) => {
-    //     if (!err) {
-    //       res.json({
-    //         status: res.statusCode,
-    //         msg: "Product Added ",
-    //       });
-    //     } else {
-    //       res.json({
-    //         status: res.statusCode,
-    //         msg: "An error occured",
-    //         err:err
-    //       });
-    //     }
-    //   });
-    // }
-  
-addProduct(req, res) {
-  const { prodID,prodName, categoryID, price, prodUrl } = req.body; // Assuming these are the columns in your table
-  const query = `
-    INSERT INTO Products (prodName, categoryID, price, prodUrl)
-    VALUES (?, ?, ?, ?)
-  `;
-  db.query(query, [prodID,prodName, categoryID, price, prodUrl], (err) => {
-    if (!err) {
-      res.json({
-        status: res.statusCode,
-        msg: "Product Added",
-      });
-    } else {
-      res.json({
-        status: res.statusCode,
-        msg: "An error occurred",
-        err: err,
+    addProduct(req, res) {
+      const query = `
+              INSERT INTO Products 
+              SET ?
+          `;
+      db.query(query, [req.body], (err) => {
+        if (!err) {
+          res.json({
+            status: res.statusCode,
+            msg: "Product Added ",
+          });
+        } else {
+          res.json({
+            status: res.statusCode,
+            msg: "An error occured",
+            err:err
+          });
+        }
       });
     }
-  });
-}
+  
+// addProduct(req, res) {
+//   const { prodID,prodName, categoryID, price, prodUrl } = req.body; // Assuming these are the columns in your table
+//   const query = `
+//     INSERT INTO Products (prodName, categoryID, price, prodUrl)
+//     VALUES (?, ?, ?, ?)
+//   `;
+//   db.query(query, [prodID,prodName, categoryID, price, prodUrl], (err) => {
+//     if (!err) {
+//       res.json({
+//         status: res.statusCode,
+//         msg: "Product Added",
+//       });
+//     } else {
+//       res.json({
+//         status: res.statusCode,
+//         msg: "An error occurred",
+//         err: err,
+//       });
+//     }
+//   });
+// }
 
 
    

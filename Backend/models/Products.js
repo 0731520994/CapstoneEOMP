4ac09a1973800f0568f31ca3402945daae14fcad
Mongikazi Dlanjwa <129971002+0731520994@users.noreply.github.com>
2023-09-13 21:54:19 +0200

    Update Products.js

--- Backend/models/Products.js
+++ Backend/models/Products.js
@@ -114,22 +114,43 @@ class Products{
         });
       }
     
-
-deleteProduct(req,res){
-    const query = `
-    DELETE FROM Products
-    WHERE prodID = ?;
-    `
-    const id = req.params.id
-    db.query(query, [id],
-        (err, )=>{
-            if (err) throw err
+      deleteProduct(req, res) {
+        const query = `
+                DELETE FROM Products
+                WHERE prodID = ${req.params.prodID};
+            `;
+        db.query(query, [req.body], (err) => {
+          if (!err) {
             res.json({
-              status:  res.statusCode,
-              msg: 'Product was deleted'
-             })
-        })
- }
-}
+              status: res.statusCode,
+              msg: "Product was deleted! ",
+            });
+          } else {
+            res.json({
+              status: res.statusCode,
+              msg: "An error occured",
+              err:err
+            });
+          }
+        });
+      }
+    
+
+// deleteProduct(req,res){
+//     const query = `
+//     DELETE FROM Products
+//     WHERE prodID = ?;
+//     `
+//     const id = req.params.id
+//     db.query(query, [id],
+//         (err, )=>{
+//             if (err) throw err
+//             res.json({
+//               status:  res.statusCode,
+//               msg: 'Product was deleted'
+//              })
+//         })
+//  }
+// }
 
 module.exports = Products

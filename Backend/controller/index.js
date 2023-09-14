864a3557dae3b535e41a6855006fd87e123640b6
Mongikazi Dlanjwa <129971002+0731520994@users.noreply.github.com>
2023-09-14 23:38:14 +0200

    Update index.js

--- Backend/controller/index.js
+++ Backend/controller/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const {verifyToken} = require ('../middleware/AuthonticateUser')
+const {verifyToken} = require ('../middleware/AuthenticateUser')
 const path =require('path')
 const routes = express.Router()
 
